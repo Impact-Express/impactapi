9144fbdf67c587b1381c2ad8a3697443e0e9f18e
Richard Bailey <richard@impactexpress.co.uk>
2019-12-05 10:34:54 +0000

    dissable registration

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::get('/', 'HomeController@index');
 Route::get('/home', 'HomeController@index')->name('home');
